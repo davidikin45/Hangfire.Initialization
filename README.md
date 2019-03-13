2b00e12a14a2ed890051687cbec3e2e67022f019
David Ikin <davidikin45@gmail.com>
2019-03-13 17:41:14 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,100 @@
+# Hangfire Initialization for SQL Server and SQLite
+
+* I have created the following extension methods which aims to allow Hangfire to be initialized & destroyed independently in a similar manner to how EF Core initialization works. A key addition is the ability to create a new database if it doesn't already exist.
+* Supports SQL Server and SQLite connection strings.
+* It also promotes [db initialization within program.cs Main method rather than startup class](https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro?view=aspnetcore-2.2). This is good practice especially when using the [WebApplicationFactory for Integration Tests](https://docs.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-2.2).
+
+## Usage
+* await HangfireInitializer.EnsureTablesDeletedAsync(connectionString) = Ensures only tables related to hangfire are deleted.
+* await HangfireInitializer.EnsureDbCreatedAsync(connectionString) = Ensures hangfire physical database is created in preparation for hangfire to create schema.
+* await HangfireInitializer.EnsureDbAndTablesCreatedAsync(connectionString) = Ensures hangfire physical database is created and tables created. [See docs](http://docs.hangfire.io/en/latest/configuration/using-sql-server.html)
+* await HangfireInitializer.EnsureDbDestroyedAsync(connectionString) = Deletes physical database.
+* Ensure Language Version is set to 'C# latest minor version (latest) to allow async Main.
+* Ensure Main method is async.
+```
+ public class Program
+{
+	public static async Task Main(string[] args)
+	{
+		
+	}
+}
+```
+
+## Development and Integration Environment Example
+```
+var sqlServerConnectionString = "Server=(localdb)\\mssqllocaldb;Database=HangfireDatabase;Trusted_Connection=True;MultipleActiveResultSets=true;";
+await HangfireInitializer.EnsureTablesDeleted(sqlServerConnectionString);
+await HangfireInitializer.EnsureDbAndTablesCreatedAsync(sqlServerConnectionString);
+
+var sqliteConnectionString = "Data Source=Hangfire.db;";
+await HangfireInitializer.EnsureTablesDeletedAsync(sqliteConnectionString);
+await HangfireInitializer.EnsureDbAndTablesCreatedAsync(sqliteConnectionString);
+```
+
+## Staging and Production Environment Example
+```
+var sqlServerConnectionString = "Server=(localdb)\\mssqllocaldb;Database=HangfireDatabase;Trusted_Connection=True;MultipleActiveResultSets=true;";
+await HangfireInitializer.EnsureDbAndTablesCreatedAsync(sqlServerConnectionString);
+
+var sqliteConnectionString = "Data Source=Hangfire.db;";
+await HangfireInitializer.EnsureDbAndTablesCreatedAsync(sqliteConnectionString);
+```
+
+## Example
+```
+public static async Task Main(string[] args)
+{
+     var host = CreateWebHostBuilder(args).Build();
+
+    using (var scope = host.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        try
+        {
+            var configuration = services.GetRequiredService<IConfiguration>();
+			var connectionString = configuration.GetConnectionString("DefaultConnection");
+            await HangfireInitializer.EnsureTablesDeletedAsync(connectionString);
+			await HangfireInitializer.EnsureDbAndTablesCreatedAsync(connectionString);
+        }
+        catch (Exception ex)
+        {
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An error occurred while initializing Hangfire db.");
+        }
+    }
+
+    host.Run();
+}
+
+public class Startup
+{
+     public IConfiguration Configuration { get; }
+
+	 public Startup(IConfiguration configuration)
+	{
+		Configuration = configuration;
+	}
+	
+	public virtual void ConfigureServices(IServiceCollection services)
+	{
+		var connectionString = Configuration.GetConnectionString("DefaultConnection");
+		services.AddHangfire(config =>
+		{
+			var options = new SqlServerStorageOptions
+			{
+				//Won't try and create database tables
+				PrepareSchemaIfNecessary = false
+			};
+
+			config.UseSqlServerStorage(connectionString, options);
+		});
+	}
+
+	public void Configure(IApplicationBuilder app)
+	{
+		app.UseHangfireDashboard();
+		app.UseHangfireServer();
+	}
+}
+```
---
+++ src/Hangfire.Initialization/HangfireInitializer.cs
@@ -0,0 +1,189 @@
+﻿using Database.Initialization;
+using Hangfire.SQLite;
+using Hangfire.SqlServer;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hangfire.Initialization
+{
+    public static class HangfireInitializer
+    {
+        public static async Task EnsureTablesDeletedAsync(string connectionString, CancellationToken cancellationToken = default)
+        {
+            var tableNames = new List<string>();
+
+            tableNames.Add($"[HangFire].[AggregatedCounter]");
+            tableNames.Add($"[HangFire].[Counter]");
+            tableNames.Add($"[HangFire].[Hash]");
+            tableNames.Add($"[HangFire].[Job]");
+            tableNames.Add($"[HangFire].[JobParameter]");
+            tableNames.Add($"[HangFire].[JobQueue]");
+            tableNames.Add($"[HangFire].[List]");
+            tableNames.Add($"[HangFire].[Schema]");
+            tableNames.Add($"[HangFire].[Server]");
+            tableNames.Add($"[HangFire].[Set]");
+            tableNames.Add($"[HangFire].[State]");
+
+            var commands = new List<String>();
+            if (string.IsNullOrEmpty(connectionString))
+            {
+
+            }
+            else if (ConnectionStringHelper.IsSQLite(connectionString))
+            {
+                bool dbExists = await DbInitializer.ExistsAsync(connectionString, cancellationToken);
+
+                if (dbExists)
+                {
+                    using (var conn = new SqliteConnection(connectionString))
+                    {
+                        await conn.OpenAsync(cancellationToken);
+
+                        using (SqliteTransaction transaction = conn.BeginTransaction())
+                        {
+                            //Drop tables
+                            foreach (var tableName in tableNames)
+                            {
+                                foreach (var t in tableNames)
+                                {
+                                    try
+                                    {
+                                        var commandSql = $"DROP TABLE IF EXISTS {t.Replace("].[", ".")};";
+                                        using (var command = new SqliteCommand(commandSql, conn, transaction))
+                                        {
+                                            await command.ExecuteNonQueryAsync(cancellationToken);
+                                        }
+
+                                        commands.Add(commandSql);
+                                    }
+                                    catch
+                                    {
+
+                                    }
+                                }
+                            }
+
+                            transaction.Rollback();
+                        }
+
+                        using (SqliteTransaction transaction = conn.BeginTransaction())
+                        {
+                            foreach (var commandSql in commands)
+                            {
+                                using (var command = new SqliteCommand(commandSql, conn, transaction))
+                                {
+                                    await command.ExecuteNonQueryAsync(cancellationToken);
+                                }
+                            }
+
+                            transaction.Commit();
+                        }
+                    }
+                }
+                else
+                {
+                    await DbInitializer.EnsureDestroyedAsync(connectionString, cancellationToken);
+                }
+            }
+            else
+            {
+                bool dbExists = await DbInitializer.ExistsAsync(connectionString, cancellationToken);
+
+                if (dbExists)
+                {
+                    using (var conn = new SqlConnection(connectionString))
+                    {
+                        await conn.OpenAsync(cancellationToken);
+
+                        using (SqlTransaction transaction = conn.BeginTransaction())
+                        {
+                            //Drop tables
+                            foreach (var tableName in tableNames)
+                            {
+                                foreach (var t in tableNames)
+                                {
+                                    try
+                                    {
+                                        var commandSql = $"DROP TABLE IF EXISTS {t}";
+                                        using (var command = new SqlCommand(commandSql, conn, transaction))
+                                        {
+                                            await command.ExecuteNonQueryAsync(cancellationToken);
+                                        }
+
+                                        commands.Add(commandSql);
+                                    }
+                                    catch
+                                    {
+
+                                    }
+                                }
+                            }
+
+                            transaction.Rollback();
+                        }
+
+                        using (SqlTransaction transaction = conn.BeginTransaction())
+                        {
+                            foreach (var commandSql in commands)
+                            {
+                                using (var command = new SqlCommand(commandSql, conn, transaction))
+                                {
+                                    await command.ExecuteNonQueryAsync(cancellationToken);
+                                }
+                            }
+
+                            transaction.Commit();
+                        }
+                    }
+                }
+                else
+                {
+                    await DbInitializer.EnsureDestroyedAsync(connectionString, cancellationToken);
+                }
+            }
+        }
+
+        public static Task<bool> EnsureDbCreatedAsync(string connectionString, CancellationToken cancellationToken = default)
+        {
+            return DbInitializer.EnsureCreatedAsync(connectionString, cancellationToken);
+        }
+
+        public static async Task EnsureDbAndTablesCreatedAsync(string connectionString, CancellationToken cancellationToken = default)
+        {
+            await EnsureDbCreatedAsync(connectionString, cancellationToken);
+            if (string.IsNullOrEmpty(connectionString))
+            {
+
+            }
+            else if (ConnectionStringHelper.IsSQLite(connectionString))
+            {
+                var options = new SQLiteStorageOptions
+                {
+                    PrepareSchemaIfNecessary = true
+                };
+
+                //Initialize Schema
+                var storage = new SQLiteStorage(connectionString, options);
+            }
+            else
+            {
+                var options = new SqlServerStorageOptions
+                {
+                    PrepareSchemaIfNecessary = true
+                };
+
+                //Initialize Schema
+                var storage = new SqlServerStorage(connectionString, options);
+            }
+        }
+
+        public static Task EnsureDbDestroyedAsync(string connectionString, CancellationToken cancellationToken = default)
+        {
+            return DbInitializer.EnsureDestroyedAsync(connectionString, cancellationToken);
+        }
+    }
+}
