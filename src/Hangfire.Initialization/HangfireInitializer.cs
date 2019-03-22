ee666b07fb541227e5aab443813f9405d6eab21d
David Ikin <davidikin45@gmail.com>
2019-03-22 14:52:47 +0000

    changes

--- src/Hangfire.Initialization/HangfireInitializer.cs
+++ src/Hangfire.Initialization/HangfireInitializer.cs
@@ -4,6 +4,7 @@ using Hangfire.SqlServer;
 using Microsoft.Data.Sqlite;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading;
@@ -27,140 +28,239 @@ namespace Hangfire.Initialization
             ("HangFire", "State"),
             ("HangFire", "Job")
         };
-        public static async Task<bool> EnsureTablesDeletedAsync(string connectionString, CancellationToken cancellationToken = default)
+
+        #region Ensure Db and Tables Created
+        public static async Task<bool> EnsureDbAndTablesCreatedAsync(string connectionString, CancellationToken cancellationToken = default)
         {
-            var commands = new List<String>();
             if (string.IsNullOrEmpty(connectionString))
             {
                 return false;
             }
-            else if (Database.Initialization.ConnectionStringHelper.IsSQLite(connectionString))
+            else if (ConnectionStringHelper.IsSQLite(connectionString))
             {
-                bool dbExists = await DbInitializer.ExistsAsync(connectionString, cancellationToken).ConfigureAwait(false);
-
-                if (dbExists)
+                using (var connection = new SqliteConnection(connectionString))
                 {
-                    var persistedTables = await DbInitializer.TablesAsync(connectionString, cancellationToken).ConfigureAwait(false);
-
-                    using (var conn = new SqliteConnection(connectionString))
-                    {
-                        await conn.OpenAsync(cancellationToken).ConfigureAwait(false);
-
-                        var deleteTables = Tables.Where(x => persistedTables.Any(p => (p.TableName == x.TableName || p.TableName == $"{x.Schema}.{x.TableName}") && (p.Schema == x.Schema || string.IsNullOrEmpty(p.Schema))));
-
-                        //Drop tables
-                        foreach (var t in deleteTables)
-                        {
-                            var commandSql = $"DROP TABLE [{t.Schema}.{t.TableName}];";
-                            commands.Add(commandSql);
-                        }
-
-                        bool deleted = false;
-                        using (SqliteTransaction transaction = conn.BeginTransaction())
-                        {
-                            foreach (var commandSql in commands)
-                            {
-                                deleted = true;
-                                using (var command = new SqliteCommand(commandSql, conn, transaction))
-                                {
-                                    await command.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
-                                }
-                            }
-
-                            transaction.Commit();
-                        }
-
-                        return deleted;
-                    }
-                }
-                else
-                {
-                    return await DbInitializer.EnsureDestroyedAsync(connectionString, cancellationToken).ConfigureAwait(false);
+                    return await EnsureDbAndTablesCreatedAsync(connection, cancellationToken);
                 }
             }
             else
             {
-                bool dbExists = await DbInitializer.ExistsAsync(connectionString, cancellationToken);
-
-                if (dbExists)
+                using (var connection = new SqlConnection(connectionString))
                 {
-                    var persistedTables = await DbInitializer.TablesAsync(connectionString, cancellationToken).ConfigureAwait(false);
-
-                    using (var conn = new SqlConnection(connectionString))
-                    {
-                        conn.Open();
-
-                        var deleteTables = Tables.Where(x => persistedTables.Any(p => (p.TableName == x.TableName || p.TableName == $"{x.Schema}.{x.TableName}") && (p.Schema == x.Schema || string.IsNullOrEmpty(p.Schema))));
-
-                        //Drop tables
-                        foreach (var t in deleteTables)
-                        {
-                            var commandSql = $"DROP TABLE [{t.Schema}].[{t.TableName}];";
-                            commands.Add(commandSql);
-                        }
-
-                        bool deleted = false;
-                        using (SqlTransaction transaction = conn.BeginTransaction())
-                        {
-                            foreach (var commandSql in commands)
-                            {
-                                deleted = true;
-                                using (var command = new SqlCommand(commandSql, conn, transaction))
-                                {
-                                    await command.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
-                                }
-                            }
-
-                            transaction.Commit();
-                        }
-
-                        return deleted;
-                    }
-                }
-                else
-                {
-                    return await DbInitializer.EnsureDestroyedAsync(connectionString, cancellationToken).ConfigureAwait(false);
+                    return await EnsureDbAndTablesCreatedAsync(connection, cancellationToken);
                 }
             }
         }
 
-        public static Task<bool> EnsureDbCreatedAsync(string connectionString, CancellationToken cancellationToken = default)
+        public static async Task<bool> EnsureDbAndTablesCreatedAsync(DbConnection existingConnection, CancellationToken cancellationToken = default)
         {
-            return DbInitializer.EnsureCreatedAsync(connectionString, cancellationToken);
-        }
-
-        public static async Task EnsureDbAndTablesCreatedAsync(string connectionString, CancellationToken cancellationToken = default)
-        {
-            await EnsureDbCreatedAsync(connectionString, cancellationToken).ConfigureAwait(false);
-            if (string.IsNullOrEmpty(connectionString))
+            if (existingConnection is SqliteConnection)
             {
+                await EnsureDbCreatedAsync(existingConnection, cancellationToken).ConfigureAwait(false);
 
-            }
-            else if (ConnectionStringHelper.IsSQLite(connectionString))
-            {
+                var persistedTables = await DbInitializer.TablesAsync(existingConnection, cancellationToken);
+
                 var options = new SQLiteStorageOptions
                 {
                     PrepareSchemaIfNecessary = true
                 };
 
                 //Initialize Schema
-                var storage = new SQLiteStorage(connectionString, options);
+                var storage = new SQLiteStorage((SqliteConnection)existingConnection, options);
+
+                return !persistedTables.Any(x => x.TableName.Contains("AggregatedCounter"));
             }
-            else
+            else if (existingConnection is SqlConnection)
             {
+                await EnsureDbCreatedAsync(existingConnection, cancellationToken).ConfigureAwait(false);
+
+                var persistedTables = await DbInitializer.TablesAsync(existingConnection, cancellationToken);
+
                 var options = new SqlServerStorageOptions
                 {
                     PrepareSchemaIfNecessary = true
                 };
 
                 //Initialize Schema
-                var storage = new SqlServerStorage(connectionString, options);
+                var storage = new SqlServerStorage(existingConnection, options);
+
+                return !persistedTables.Any(x => x.TableName.Contains("AggregatedCounter"));
+            }
+            else
+            {
+                throw new Exception("Unsupported Connection");
+            }
+        }
+        #endregion
+
+        #region Ensure Db Created
+        public static Task<bool> EnsureDbCreatedAsync(string connectionString, CancellationToken cancellationToken = default)
+        {
+            return DbInitializer.EnsureCreatedAsync(connectionString, cancellationToken);
+        }
+
+        public static Task<bool> EnsureDbCreatedAsync(DbConnection existingConnection, CancellationToken cancellationToken = default)
+        {
+            return DbInitializer.EnsureCreatedAsync(existingConnection, cancellationToken);
+        }
+        #endregion
+
+        #region Ensure Tables Deleted
+        public static async Task<bool> EnsureTablesDeletedAsync(string connectionString, CancellationToken cancellationToken = default)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                return false;
+            }
+            else if (ConnectionStringHelper.IsSQLite(connectionString))
+            {
+                using (var connection = new SqliteConnection(connectionString))
+                {
+                    return await EnsureDbAndTablesCreatedAsync(connection, cancellationToken);
+                }
+            }
+            else
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    return await EnsureDbAndTablesCreatedAsync(connection, cancellationToken);
+                }
             }
         }
 
+        public static async Task<bool> EnsureTablesDeletedAsync(DbConnection existingConnection, CancellationToken cancellationToken = default)
+        {
+            var commands = new List<String>();
+            if (existingConnection is SqliteConnection)
+            {
+                bool dbExists = await DbInitializer.ExistsAsync(existingConnection, cancellationToken).ConfigureAwait(false);
+
+                if (dbExists)
+                {
+                    var persistedTables = await DbInitializer.TablesAsync(existingConnection, cancellationToken).ConfigureAwait(false);
+
+                    var opened = false;
+                    if(existingConnection.State != System.Data.ConnectionState.Open)
+                    {
+                        await existingConnection.OpenAsync(cancellationToken).ConfigureAwait(false);
+                        opened = true;
+                    }
+
+                    var deleteTables = Tables.Where(x => persistedTables.Any(p => (p.TableName == x.TableName || p.TableName == $"{x.Schema}.{x.TableName}") && (p.Schema == x.Schema || string.IsNullOrEmpty(p.Schema))));
+
+                    //Drop tables
+                    foreach (var t in deleteTables)
+                    {
+                        var commandSql = $"DROP TABLE [{t.Schema}.{t.TableName}];";
+                        commands.Add(commandSql);
+                    }
+
+                    bool deleted = false;
+
+                    try
+                    {
+                        using (SqliteTransaction transaction = ((SqliteConnection)existingConnection).BeginTransaction())
+                        {
+                            foreach (var commandSql in commands)
+                            {
+                                deleted = true;
+                                using (var command = new SqliteCommand(commandSql, ((SqliteConnection)existingConnection), transaction))
+                                {
+                                    await command.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
+                                }
+                            }
+
+                            transaction.Commit();
+                        }
+                    }
+                    finally
+                    {
+                        if(opened && existingConnection.State == System.Data.ConnectionState.Open)
+                        {
+                            existingConnection.Close();
+                        }
+                    }
+
+                    return deleted;
+                }
+                else
+                {
+                    return await DbInitializer.EnsureDestroyedAsync(existingConnection, cancellationToken).ConfigureAwait(false);
+                }
+            }
+            else if (existingConnection is SqlConnection)
+            {
+                bool dbExists = await DbInitializer.ExistsAsync(existingConnection, cancellationToken);
+
+                if (dbExists)
+                {
+                    var persistedTables = await DbInitializer.TablesAsync(existingConnection, cancellationToken).ConfigureAwait(false);
+
+                    var opened = false;
+                    if (existingConnection.State != System.Data.ConnectionState.Open)
+                    {
+                        await existingConnection.OpenAsync(cancellationToken).ConfigureAwait(false);
+                        opened = true;
+                    }
+
+                    var deleteTables = Tables.Where(x => persistedTables.Any(p => (p.TableName == x.TableName || p.TableName == $"{x.Schema}.{x.TableName}") && (p.Schema == x.Schema || string.IsNullOrEmpty(p.Schema))));
+
+                    //Drop tables
+                    foreach (var t in deleteTables)
+                    {
+                        var commandSql = $"DROP TABLE [{t.Schema}].[{t.TableName}];";
+                        commands.Add(commandSql);
+                    }
+
+                    bool deleted = false;
+                    try
+                    {
+                        using (SqlTransaction transaction = ((SqlConnection)existingConnection).BeginTransaction())
+                        {
+                            foreach (var commandSql in commands)
+                            {
+                                deleted = true;
+                                using (var command = new SqlCommand(commandSql, ((SqlConnection)existingConnection), transaction))
+                                {
+                                    await command.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
+                                }
+                            }
+
+                            transaction.Commit();
+                        }
+                    }
+                    finally
+                    {
+                        if(opened && existingConnection.State == System.Data.ConnectionState.Open)
+                        {
+                            existingConnection.Close();
+                        }
+                    }
+
+                    return deleted;
+                }
+                else
+                {
+                    return await DbInitializer.EnsureDestroyedAsync(existingConnection, cancellationToken).ConfigureAwait(false);
+                }
+            }
+            else
+            {
+                throw new Exception("Unsupported Connection");
+            }
+        }
+        #endregion
+
+        #region Ensure Db Destroyed
         public static Task<bool> EnsureDbDestroyedAsync(string connectionString, CancellationToken cancellationToken = default)
         {
             return DbInitializer.EnsureDestroyedAsync(connectionString, cancellationToken);
         }
+
+        public static Task<bool> EnsureDbDestroyedAsync(DbConnection existingConnection, CancellationToken cancellationToken = default)
+        {
+            return DbInitializer.EnsureDestroyedAsync(connection, cancellationToken);
+        }
+        #endregion
     }
 }
