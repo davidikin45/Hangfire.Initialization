f1025fecd5b360f7f7fa12a7beffc49b7adee573
David Ikin <davidikin45@gmail.com>
2019-07-16 15:56:44 +0100

    changes

---
+++ src/Hangfire.Initialization/HangfireBackgroundJobServer.cs
@@ -0,0 +1,136 @@
+﻿using Hangfire.Annotations;
+using Hangfire.Client;
+using Hangfire.Common;
+using Hangfire.Logging;
+using Hangfire.Server;
+using Hangfire.States;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Hangfire.Initialization
+{
+    public class HangfireBackgroundJobServer : IBackgroundProcessingServer
+    {
+        private readonly ILog _logger = LogProvider.For<BackgroundJobServer>();
+        private readonly BackgroundJobServerOptions _options;
+        private readonly BackgroundProcessingServer _processingServer;
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        public HangfireBackgroundJobServer(
+            [NotNull] BackgroundJobServerOptions options,
+            [NotNull] JobStorage storage,
+            [NotNull] IEnumerable<IBackgroundProcess> additionalProcesses,
+            [CanBeNull] IJobFilterProvider filterProvider,
+            [CanBeNull] JobActivator activator,
+            [CanBeNull] IBackgroundJobFactory factory,
+            [CanBeNull] IBackgroundJobPerformer performer,
+            [CanBeNull] IBackgroundJobStateChanger stateChanger)
+        {
+            if (storage == null) throw new ArgumentNullException(nameof(storage));
+            if (options == null) throw new ArgumentNullException(nameof(options));
+            if (additionalProcesses == null) throw new ArgumentNullException(nameof(additionalProcesses));
+
+            _options = options;
+
+            var processes = new List<IBackgroundProcessDispatcherBuilder>();
+            processes.AddRange(GetRequiredProcesses(filterProvider, activator, factory, performer, stateChanger));
+            processes.AddRange(additionalProcesses.Select(x => x.UseBackgroundPool(1)));
+
+            var properties = new Dictionary<string, object>
+            {
+                { "Queues", options.Queues },
+                { "WorkerCount", options.WorkerCount }
+            };
+
+            _logger.Info($"Starting Hangfire Server using job storage: '{storage}'");
+
+            storage.WriteOptionsToLog(_logger);
+
+            _logger.Info("Using the following options for Hangfire Server:\r\n" +
+                $"    Worker count: {options.WorkerCount}\r\n" +
+                $"    Listening queues: {String.Join(", ", options.Queues.Select(x => "'" + x + "'"))}\r\n" +
+                $"    Shutdown timeout: {options.ShutdownTimeout}\r\n" +
+                $"    Schedule polling interval: {options.SchedulePollingInterval}");
+
+            _processingServer = new BackgroundProcessingServer(
+                storage,
+                processes,
+                properties,
+                GetProcessingServerOptions());
+        }
+
+        public void Dispose()
+        {
+            _processingServer.Dispose();
+        }
+
+        public void SendStop()
+        {
+            _logger.Debug("Hangfire Server is stopping...");
+            _processingServer.SendStop();
+        }
+
+        public bool WaitForShutdown(TimeSpan timeout)
+        {
+            return _processingServer.WaitForShutdown(timeout);
+        }
+
+        public Task WaitForShutdownAsync(CancellationToken cancellationToken)
+        {
+            return _processingServer.WaitForShutdownAsync(cancellationToken);
+        }
+
+        private IEnumerable<IBackgroundProcessDispatcherBuilder> GetRequiredProcesses(
+           [CanBeNull] IJobFilterProvider filterProvider,
+           [CanBeNull] JobActivator activator,
+           [CanBeNull] IBackgroundJobFactory factory,
+           [CanBeNull] IBackgroundJobPerformer performer,
+           [CanBeNull] IBackgroundJobStateChanger stateChanger)
+        {
+            var processes = new List<IBackgroundProcessDispatcherBuilder>();
+            var timeZoneResolver = _options.TimeZoneResolver ?? new DefaultTimeZoneResolver();
+
+            if (factory == null && performer == null && stateChanger == null)
+            {
+                filterProvider = filterProvider ?? _options.FilterProvider ?? JobFilterProviders.Providers;
+                activator = activator ?? _options.Activator ?? JobActivator.Current;
+
+                factory = new BackgroundJobFactory(filterProvider);
+                performer = new BackgroundJobPerformer(filterProvider, activator, _options.TaskScheduler);
+                stateChanger = new BackgroundJobStateChanger(filterProvider);
+            }
+            else
+            {
+                if (factory == null) throw new ArgumentNullException(nameof(factory));
+                if (performer == null) throw new ArgumentNullException(nameof(performer));
+                if (stateChanger == null) throw new ArgumentNullException(nameof(stateChanger));
+            }
+
+            processes.Add(new Worker(_options.Queues, performer, stateChanger).UseBackgroundPool(_options.WorkerCount));
+            processes.Add(new DelayedJobScheduler(_options.SchedulePollingInterval, stateChanger).UseBackgroundPool(1));
+            processes.Add(new RecurringJobScheduler(factory, _options.SchedulePollingInterval, timeZoneResolver).UseBackgroundPool(1));
+
+            return processes;
+        }
+
+        private BackgroundProcessingServerOptions GetProcessingServerOptions()
+        {
+            return new BackgroundProcessingServerOptions
+            {
+                StopTimeout = _options.StopTimeout,
+                ShutdownTimeout = _options.ShutdownTimeout,
+                HeartbeatInterval = _options.HeartbeatInterval,
+#pragma warning disable 618
+                ServerCheckInterval = _options.ServerWatchdogOptions?.CheckInterval ?? _options.ServerCheckInterval,
+                ServerTimeout = _options.ServerWatchdogOptions?.ServerTimeout ?? _options.ServerTimeout,
+#pragma warning restore 618
+                CancellationCheckInterval = _options.CancellationCheckInterval,
+                ServerName = _options.ServerName
+            };
+        }
+    }
+}
--- src/Hangfire.Initialization/HangfireLauncher.cs
+++ src/Hangfire.Initialization/HangfireLauncher.cs
@@ -5,48 +5,74 @@ using Hangfire.Server;
 using Hangfire.SQLite;
 using Hangfire.SqlServer;
 using Hangfire.States;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Data.Sqlite;
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
 
 namespace Hangfire.Initialization
 {
     public static class HangfireLauncher
     {
-        public static (BackgroundJobServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient) StartHangfireServerInMemory()
+        public static (IBackgroundProcessingServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient, JobStorage Storage) StartHangfireServerInMemory(Action<HangfireLauncherOptions> config = null)
         {
-            return StartHangfireServer(new BackgroundJobServerOptions(), "", true);
+            return StartHangfireServer(new BackgroundJobServerOptions(), "", config);
         }
 
-        public static (BackgroundJobServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient) StartHangfireServerInMemory(string serverName)
+        public static (IBackgroundProcessingServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient, JobStorage Storage) StartHangfireServerInMemory(string serverName, Action<HangfireLauncherOptions> config = null)
         {
-            return StartHangfireServer(serverName, "");
+            return StartHangfireServer(serverName, "", config);
         }
 
-        public static (BackgroundJobServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient) StartHangfireServerSQLiteInMemory(bool prepareSchemaIfNecessary = true, string schemaName = "HangFire")
+        public static (IBackgroundProcessingServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient, JobStorage Storage) StartHangfireServerSQLiteInMemory(Action<HangfireLauncherOptions> config = null)
         {
-            return StartHangfireServer(new BackgroundJobServerOptions(), "DataSource=:memory:;", prepareSchemaIfNecessary, schemaName);
+            return StartHangfireServer(new BackgroundJobServerOptions(), "DataSource=:memory:;", config);
         }
 
-        public static (BackgroundJobServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient) StartHangfireServerSQLiteInMemory(string serverName, bool prepareSchemaIfNecessary = true, string schemaName = "HangFire")
+        public static (IBackgroundProcessingServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient, JobStorage Storage) StartHangfireServerSQLiteInMemory(string serverName, Action<HangfireLauncherOptions> config = null)
         {
-            return StartHangfireServer(serverName, "DataSource=:memory:;", prepareSchemaIfNecessary, schemaName);
+            return StartHangfireServer(serverName, "DataSource=:memory:;", config);
         }
 
-        public static (BackgroundJobServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient) StartHangfireServer(string serverName, string connectionString, bool prepareSchemaIfNecessary = true, string schemaName = "HangFire")
+        public static (IBackgroundProcessingServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient, JobStorage Storage) StartHangfireServer(string serverName, string connectionString, Action<HangfireLauncherOptions> config = null)
         {
             var options = new BackgroundJobServerOptions
             {
                 ServerName = serverName,
                 Queues = new string[] { serverName, "default" }
             };
-            return StartHangfireServer(options, connectionString, prepareSchemaIfNecessary, schemaName);
+            return StartHangfireServer(options, connectionString, config);
         }
 
-        public static (BackgroundJobServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient) StartHangfireServer(BackgroundJobServerOptions options, string connectionString, bool prepareSchemaIfNecessary, string schemaName = "HangFire")
+        public static (IBackgroundProcessingServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient, JobStorage Storage) StartHangfireServer(string serverName, DbConnection existingConnection, Action<HangfireLauncherOptions> config = null)
         {
+            var options = new BackgroundJobServerOptions
+            {
+                ServerName = serverName,
+                Queues = new string[] { serverName, "default" }
+            };
+            return StartHangfireServer(options, existingConnection, config);
+        }
+
+        public static (IBackgroundProcessingServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient, JobStorage Storage) StartHangfireServer(BackgroundJobServerOptions options, JobStorage storage, Action<HangfireLauncherOptions> config = null)
+        {
+            var launcherOptions = new HangfireLauncherOptions();
+            if (config != null)
+                config(launcherOptions);
+
+            return StartHangfireServer(options, storage, launcherOptions);
+        }
+
+        public static (IBackgroundProcessingServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient, JobStorage Storage) StartHangfireServer(BackgroundJobServerOptions options, string connectionString, Action<HangfireLauncherOptions> config = null)
+        {
+            var launcherOptions = new HangfireLauncherOptions();
+            if (config != null)
+                config(launcherOptions);
+
             JobStorage storage;
             if (string.IsNullOrWhiteSpace(connectionString))
             {
@@ -56,8 +82,8 @@ namespace Hangfire.Initialization
             {
                 var storageOptions = new SQLiteStorageOptions()
                 {
-                    SchemaName = schemaName,
-                    PrepareSchemaIfNecessary = prepareSchemaIfNecessary
+                    SchemaName = launcherOptions.SchemaName,
+                    PrepareSchemaIfNecessary = launcherOptions.PrepareSchemaIfNecessary
                 };
                 storage = new SQLiteStorage(connectionString, storageOptions);
             }
@@ -65,48 +91,43 @@ namespace Hangfire.Initialization
             {
                 var storageOptions = new SqlServerStorageOptions()
                 {
-                    SchemaName = schemaName,
-                    PrepareSchemaIfNecessary = prepareSchemaIfNecessary
+                    SchemaName = launcherOptions.SchemaName,
+                    PrepareSchemaIfNecessary = launcherOptions.PrepareSchemaIfNecessary
                 };
                 storage = new SqlServerStorage(connectionString, storageOptions);
             }
 
-            return StartHangfireServer(options, storage);
-        }
-        public static (BackgroundJobServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient) StartHangfireServer(string serverName, DbConnection existingConnection, bool prepareSchemaIfNecessary = true, string schemaName = "HangFire")
-        {
-            var options = new BackgroundJobServerOptions
-            {
-                ServerName = serverName,
-                Queues = new string[] { serverName, "default" }
-            };
-            return StartHangfireServer(options, existingConnection, prepareSchemaIfNecessary, schemaName);
+            return StartHangfireServer(options, storage, launcherOptions);
         }
 
-        public static (BackgroundJobServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient) StartHangfireServer(BackgroundJobServerOptions options, DbConnection existingConnection, bool prepareSchemaIfNecessary, string schemaName = "HangFire")
+        public static (IBackgroundProcessingServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient, JobStorage Storage) StartHangfireServer(BackgroundJobServerOptions options, DbConnection existingConnection, Action<HangfireLauncherOptions> config = null)
         {
+            var launcherOptions = new HangfireLauncherOptions();
+            if (config != null)
+                config(launcherOptions);
+
             if (existingConnection is SqliteConnection)
             {
                 var storageOptions = new SQLiteStorageOptions()
                 {
-                    SchemaName = schemaName,
-                    PrepareSchemaIfNecessary = prepareSchemaIfNecessary
+                    SchemaName = launcherOptions.SchemaName,
+                    PrepareSchemaIfNecessary = launcherOptions.PrepareSchemaIfNecessary
                 };
 
                 var storage = new SQLiteStorage(existingConnection, storageOptions);
 
-                return StartHangfireServer(options, storage);
+                return StartHangfireServer(options, storage, launcherOptions);
             }
             else if (existingConnection is SqlConnection)
             {
                 var storageOptions = new SqlServerStorageOptions()
                 {
-                    SchemaName = schemaName,
-                    PrepareSchemaIfNecessary = prepareSchemaIfNecessary
+                    SchemaName = launcherOptions.SchemaName,
+                    PrepareSchemaIfNecessary = launcherOptions.PrepareSchemaIfNecessary
                 };
                 var storage = new SqlServerStorage(existingConnection, storageOptions);
 
-                return StartHangfireServer(options, storage);
+                return StartHangfireServer(options, storage, launcherOptions);
             }
             else
             {
@@ -114,28 +135,59 @@ namespace Hangfire.Initialization
             }
         }
 
-        public static (BackgroundJobServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient) StartHangfireServer(BackgroundJobServerOptions options, JobStorage storage)
+        public static (IBackgroundProcessingServer Server, IRecurringJobManager RecurringJobManager, IBackgroundJobClient BackgroundJobClient, JobStorage Storage) StartHangfireServer(BackgroundJobServerOptions options, JobStorage storage, HangfireLauncherOptions launcherOptions)
         {
-            var filterProvider = JobFilterProviders.Providers;
-            var activator = JobActivator.Current;
+            var additionalProcesses = launcherOptions?.AdditionalProcesses ?? Enumerable.Empty<IBackgroundProcess>();
 
-            var backgroundJobFactory = new BackgroundJobFactory(filterProvider);
-            var performer = new BackgroundJobPerformer(filterProvider, activator);
-            var backgroundJobStateChanger = new BackgroundJobStateChanger(filterProvider);
-            IEnumerable<IBackgroundProcess> additionalProcesses = new List<IBackgroundProcess>();
+            var filterProvider = launcherOptions?.FilterProvider ?? options.FilterProvider ?? JobFilterProviders.Providers;
+            var activator = launcherOptions?.Activator ?? options.Activator ?? JobActivator.Current;
 
-            var server = new BackgroundJobServer(options, storage, additionalProcesses,
-                options.FilterProvider ?? filterProvider,
-                options.Activator ?? activator,
-                backgroundJobFactory,
+            var factory = new BackgroundJobFactory(filterProvider);
+            var performer = new BackgroundJobPerformer(filterProvider, activator, options.TaskScheduler);
+            var stateChanger = new BackgroundJobStateChanger(filterProvider);
+
+            var server = new HangfireBackgroundJobServer(options, storage, additionalProcesses,
+                null,
+                null,
+                factory,
                 performer,
-                backgroundJobStateChanger);
+                stateChanger);
 
-            var recurringJobManager = new RecurringJobManager(storage, backgroundJobFactory);
+            if(launcherOptions?.ApplicationLifetime != null)
+            {
+                launcherOptions?.ApplicationLifetime.ApplicationStopping.Register(() => server.SendStop());
+                launcherOptions?.ApplicationLifetime.ApplicationStopped.Register(() => server.Dispose());
+            }
 
-            var backgroundJobClient = new BackgroundJobClient(storage, backgroundJobFactory, backgroundJobStateChanger);
+            var recurringJobManager = new RecurringJobManager(storage, factory);
 
-            return (server, recurringJobManager, backgroundJobClient);
+            var backgroundJobClient = new BackgroundJobClient(storage, factory, stateChanger);
+
+            return (server, recurringJobManager, backgroundJobClient, storage);
         }
+
+        public static IBackgroundJobClient GetDefaultBackgroundJobClient()
+        {
+            var clientFactoryProperty = typeof(BackgroundJob).GetProperties(BindingFlags.Instance |
+                  BindingFlags.NonPublic |
+                  BindingFlags.Public).Where(p => p.Name == "ClientFactory").First();
+
+            Func<IBackgroundJobClient> clientFactoryFunc = (Func<IBackgroundJobClient>)clientFactoryProperty.GetValue(null, null);
+            var backgroundJobClient = clientFactoryFunc();
+
+            return backgroundJobClient;
+        }
+    }
+
+    public class HangfireLauncherOptions
+    {
+        public IEnumerable<IBackgroundProcess> AdditionalProcesses { get; set; }
+        public IJobFilterProvider FilterProvider { get; set; }
+        public JobActivator Activator { get; set; }
+        public IApplicationLifetime ApplicationLifetime { get; set; }
+
+        public bool PrepareSchemaIfNecessary { get; set; } = true;
+
+        public string SchemaName { get; set; } = "HangFire";
     }
 }
