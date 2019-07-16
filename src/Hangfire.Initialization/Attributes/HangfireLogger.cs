040a0256527b32bf4245c068c2319ef60e28250c
David Ikin <davidikin45@gmail.com>
2019-07-16 19:28:51 +0100

    changes

---
+++ src/Hangfire.Initialization/Attributes/HangfireLogger.cs
@@ -0,0 +1,29 @@
+﻿using Hangfire.Common;
+using Hangfire.Logging;
+using Hangfire.Server;
+
+namespace AspNetCore.Base.Hangfire
+{
+     //services.AddHangfire(config =>
+     // {
+     //       config.UseFilter(new HangfireLoggerAttribute());
+     // });
+
+    //http://docs.hangfire.io/en/latest/extensibility/using-job-filters.html
+    public class HangfireLoggerAttribute : JobFilterAttribute,
+    IServerFilter
+    {
+        private static ILog logger = LogProvider.GetLogger("Hangfire");
+
+        public void OnPerforming(PerformingContext context)
+        {
+
+            logger.InfoFormat("Starting to perform job `{0}`", context.BackgroundJob.Id);
+        }
+
+        public void OnPerformed(PerformedContext context)
+        {
+            logger.InfoFormat("Job `{0}` has been performed", context.BackgroundJob.Id);
+        }
+    }
+}
---
+++ src/Hangfire.Initialization/Attributes/HangfireRoleAuthorizationfilter.cs
@@ -0,0 +1,44 @@
+﻿using Hangfire.Dashboard;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+
+namespace Hangfire.Initialization.Attributes
+{
+    //var options = new DashboardOptions
+    //{
+    //    Authorization = new[] { new HangfireRoleAuthorizationfilter("admin") }
+    //};
+    public class HangfireRoleAuthorizationfilter : IDashboardAuthorizationFilter
+    {
+        public string[] Roles { get; set; }
+        public bool AllowLocal { get; set; } = true;
+
+        public HangfireRoleAuthorizationfilter(params string[] roles)
+        {
+            Roles = roles;
+        }
+
+        public bool Authorize(DashboardContext context)
+        {
+            var user = GetHttpContext(context).User;
+            var isInRole = Roles.Any(role => user.IsInRole(role));
+            return (AllowLocal && (context.Request.LocalIpAddress == "127.0.0.1" || context.Request.LocalIpAddress == "::1")) || isInRole;
+        }
+
+        private HttpContext GetHttpContext(DashboardContext context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+
+            var method = context.GetType().GetProperty("HttpContext");
+
+            if (method == null)
+            {
+                throw new ArgumentException($"Context argument should have HttpContext property ");
+            }
+
+            return (HttpContext)method.GetValue(context);
+        }
+
+    }
+}
