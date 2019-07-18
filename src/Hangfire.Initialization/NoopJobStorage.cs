de994cea62b0edc4ebfdbdc27734ba2e091e7619
David Ikin <davidikin45@gmail.com>
2019-07-18 17:57:02 +0100

    changes

---
+++ src/Hangfire.Initialization/NoopJobStorage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Hangfire.Storage;
+
+namespace Hangfire.Initialization
+{
+    class NoopJobStorage : JobStorage
+    {
+        public override IStorageConnection GetConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IMonitoringApi GetMonitoringApi()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
