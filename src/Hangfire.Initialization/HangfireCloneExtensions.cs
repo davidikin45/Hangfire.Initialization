8dd609327798f864631f575859c26654315e83a7
David Ikin <davidikin45@gmail.com>
2019-07-19 04:33:08 +0100

    changes

---
+++ src/Hangfire.Initialization/HangfireCloneExtensions.cs
@@ -0,0 +1,41 @@
+﻿namespace Hangfire.Initialization
+{
+    public static class HangfireCloneExtensions
+    {
+        public static DashboardOptions Clone(this DashboardOptions options)
+        {
+            return new DashboardOptions()
+            {
+                AppPath = options.AppPath,
+                Authorization = options.Authorization,
+                IsReadOnlyFunc = options.IsReadOnlyFunc,
+                StatsPollingInterval = options.StatsPollingInterval,
+                DisplayStorageConnectionString = options.DisplayStorageConnectionString,
+                DisplayNameFunc = options.DisplayNameFunc,
+                IgnoreAntiforgeryToken = options.IgnoreAntiforgeryToken,
+                TimeZoneResolver = options.TimeZoneResolver
+            };
+        }
+
+        public static BackgroundJobServerOptions Clone(this BackgroundJobServerOptions options)
+        {
+            return new BackgroundJobServerOptions()
+            {
+                ServerName = options.ServerName,
+                WorkerCount = options.WorkerCount,
+                Queues = (string[])options.Queues?.Clone(),
+                StopTimeout = options.StopTimeout,
+                ShutdownTimeout = options.ShutdownTimeout,
+                SchedulePollingInterval = options.SchedulePollingInterval,
+                HeartbeatInterval = options.HeartbeatInterval,
+                ServerCheckInterval = options.ServerCheckInterval,
+                ServerTimeout = options.ServerTimeout,
+                CancellationCheckInterval = options.CancellationCheckInterval,
+                FilterProvider = options.FilterProvider,
+                Activator = options.Activator,
+                TimeZoneResolver = options.TimeZoneResolver,
+                TaskScheduler = options.TaskScheduler
+            };
+        }
+    }
+}
